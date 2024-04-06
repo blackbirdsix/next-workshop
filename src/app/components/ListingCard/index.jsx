aec285fadd856a485cc3616b87e07216a0dbead9
blackbirdsix <jtake6@gmail.com>
2024-04-06 15:49:01 -0700

    react dabloons website

--- src/app/components/ListingCard/index.jsx
+++ src/app/components/ListingCard/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./style.module.css";
 
-const ListingCard = () => {
+const ListingCard = ({image, name, price, button}) => {
   return (
     <div className={styles.card}>
-      
+      <img className={styles.card_img} src={image}/>
+      <h2 className={styles.card_title}>{name}</h2>
+      <p className={styles.card_price}>{price}</p>
+      <a href="#" className={styles.card_button} >{button}</a>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,52 @@
 import storeInfo from "./storeInfo";
 import styles from "./page.module.css";
+import ListingCard from "./components/ListingCard";
 
 export default function Home() {
+  const image1 = "./assets/item6.jpg";
+  const name1 = "Not a cannon";
+  const price1 = "10 Dabloons";
+  const button = "Add to Ship"
+
+  const image2 = "./assets/item3.jpg"
+  const name2 = "third leg";
+  const price2 = "5 Dabloons";
+
+  const image3 = "./assets/item4.jpg"
+  const name3 = "captain hook asmr";
+  const price3 = "100 Dabloons";
+
+  const image4 = "./assets/item5.jpg"
+  const name4 = "avatar slayer";
+  const price4 = "20 Dabloons";
+
   return (
     <main className={styles.main}>
       <h1 className={styles.store_header}>Pirate Marketplace</h1>
+      <ListingCard
+        image={image1}
+        name={name1}
+        price={price1}
+        button={button}
+      />
+      <ListingCard
+        image={image2}
+        name={name2}
+        price={price2}
+        button={button}
+      />
+      <ListingCard
+        image={image3}
+        name={name3}
+        price={price3}
+        button={button}
+      />
+      <ListingCard
+        image={image4}
+        name={name4}
+        price={price4}
+        button={button}
+      />
     </main>
   );
 }
